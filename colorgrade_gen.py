411511e55502153c52912f75e2c623b6dc14a488
lostinnowhere314 <quinlan.leishman@gmail.com>
2023-01-06 16:09:13 -0700

    proper error displaying; documentation; page colors

--- colorgrade_gen.py
+++ colorgrade_gen.py
@@ -1,5 +1,6 @@
 import numpy as np
 from collections import namedtuple
+from functools import wraps
 from colorgrade_core import *
     
 ## Page functionality
@@ -11,11 +12,30 @@ ProcessStep = namedtuple("ProcessStep", [
     "html_element",
 ])
 process_steps = []
+no_input_process_types = {'if-else', 'fill'}
 
-# get needed globals
-from js import canvas, canvas_ctx, process_items, document
+# get needed globals from the javascript
+from js import canvas, canvas_ctx, process_items, document, error_message
 
 
+# Wrapper for displaying errors to our html element
+def display_errors(fn):
+    @wraps(fn)
+    def _inner(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except Exception as e:
+            # Display the error
+            show_error_exception(
+                e, 
+                prefix=f"Error in {fn.__name__}():", 
+                post="See developer console for full stack trace."
+            )
+            # Raise anyways so that the whole stacktrace makes it to the log
+            raise
+    return _inner
+
+@display_errors
 def create_element_with_tags(el_type, **tags):
     """
     Creates a HTML element with the given tags.
@@ -45,8 +65,7 @@ def add_input_field(parent, name, label, value, size=4, **kwargs):
         "input", _type='text', name=name, id=name, value=value, size=size, **kwargs
     ))
 
-no_input_process_types = {'if-else', 'fill'}
-
+@display_errors
 def create_process_step(process_type):
     """
     Creates a process item
@@ -169,30 +188,30 @@ def create_process_step(process_type):
             "input", _type='text', name='color', id='color', value='000000', size=4
         ))
     elif process_type == "if-else":
-        add_input_field(element, 'condition', ' Condition: ', 'r+g+b > 0', size=24)
+        add_input_field(element, 'condition', ' Condition: ', 'r+g+b > 0.5', size=24)
         element.appendChild(create_element_with_tags("br"))
-        add_input_field(element, 'cond-input', 'Condition source: ', '-1', size=2)
+        add_input_field(element, 'cond-input', 'Condition source: ', '0', size=2)
         element.appendChild(create_element_with_tags("br"))
         add_input_field(element, 'true-input', 'Source if true: ', '-1', size=2)
         element.appendChild(create_element_with_tags("br"))
         add_input_field(element, 'false-input', 'Source if false: ', '-1', size=2)
         
     elif process_type == "adjust-rgb":
-        add_input_field(element, 'r-shift', ' R shift: ', 0.0, size=4)
+        add_input_field(element, 'r-shift', ' R shift: ', "0.0", size=4)
         element.appendChild(create_element_with_tags("br"))
-        add_input_field(element, 'g-shift', ' B shift: ', 0.0, size=4)
+        add_input_field(element, 'g-shift', ' B shift: ', "0.0", size=4)
         element.appendChild(create_element_with_tags("br"))
-        add_input_field(element, 'b-shift', ' G shift: ', 0.0, size=4)
+        add_input_field(element, 'b-shift', ' G shift: ', "0.0", size=4)
     elif process_type == "adjust-hsv":
-        add_input_field(element, 'h-shift', ' H shift: ', 0.0, size=4)
+        add_input_field(element, 'h-shift', ' H shift: ', "0.0", size=4)
         element.appendChild(create_element_with_tags("br"))
-        add_input_field(element, 's-shift', ' V shift: ', 0.0, size=4)
+        add_input_field(element, 's-shift', ' V shift: ', "0.0", size=4)
         element.appendChild(create_element_with_tags("br"))
-        add_input_field(element, 'v-shift', ' S shift: ', 0.0, size=4)
+        add_input_field(element, 'v-shift', ' S shift: ', "0.0", size=4)
     elif process_type == "brightness-contrast":
-        add_input_field(element, 'bright-shift', ' Brightness: ', 0.0, size=4)
+        add_input_field(element, 'bright-shift', ' Brightness: ', "0.0", size=4)
         element.appendChild(create_element_with_tags("br"))
-        add_input_field(element, 'con-shift', ' Contrast: ', 0.0, size=4)
+        add_input_field(element, 'con-shift', ' Contrast: ', "0.0", size=4)
     elif process_type == "custom":
         add_input_field(element, 'new-r', ' New R: ', 'r', size=24)
         element.appendChild(create_element_with_tags("br"))
@@ -260,6 +279,7 @@ def get_index_of_step(process_id):
     
     return None
 
+@display_errors
 def remove_process_step(process_id):
     """
     Attempts to remove the process with the given integer id.
@@ -300,6 +320,7 @@ def regenerate_display_indices():
     for i, step in enumerate(process_steps, start=1):
         step.html_element.querySelector("#process_index").innerHTML = str(i)
     
+@display_errors
 def move_step_up(process_id):
     """
     Moves the given step up one (unless it's at the beginning)
@@ -319,7 +340,9 @@ def move_step_up(process_id):
     process_items.insertBefore(step.html_element, step.html_element.previousElementSibling);
     
     regenerate_display_indices()
+    hide_error()
     
+@display_errors
 def move_step_down(process_id):
     """
     Moves the given step down one (unless it's at the end)
@@ -345,102 +368,109 @@ def generate():
     Generates the colorgrade using the given values
     note to self: in the future, add something to handle exceptions that occur
     """
-    
     cg_steps = [get_default_colorgrade()]
     
-    for process_step in process_steps:
+    for i, process_step in enumerate(process_steps, start=1):
         cg_out = None
-    
-        # Find input
-        if process_step.process_type not in no_input_process_types:
-            target_index = int(parse_arguments_from_element(process_step.html_element, ['which-step'])[0])
-            cg_in = cg_steps[target_index]
-        else:   
-            cg_in = None
+        try:
         
+            # Find input
+            if process_step.process_type not in no_input_process_types:
+                target_index = int(parse_arguments_from_element(process_step.html_element, ['which-step'])[0])
+                cg_in = cg_steps[target_index]
+            else:   
+                cg_in = None
+            
+            
+            # Run the step
+            if process_step.process_type == "8-value-recolor":
+                colors = parse_arguments_from_element(process_step.html_element, ['black','white','red','green','blue','yellow','magenta','cyan'])
+                colors_scalar = [parse_color(c) for c in colors]
+                cg_out = linear_recolor(cg_in, colors_scalar)
+                
+            elif process_step.process_type == "simple-recolor":
+                colors = parse_arguments_from_element(process_step.html_element, ['black','white'])
+                colors_scalar = [parse_color(c) for c in colors]
+                cg_out = simple_recolor(cg_in, *colors_scalar)
+                
+            elif process_step.process_type == "recenter-colors":
+                cg_out = rescale_to_fill_range(cg_in)
+                
+            elif process_step.process_type == "fill":
+                color = parse_color(
+                    parse_arguments_from_element(
+                        process_step.html_element,
+                        ['color']
+                    )[0]
+                )
+                cg_out = get_filled_colorgrade(color)
+                
+            elif process_step.process_type == "if-else":
+                condition, cond_idx, true_idx, false_idx = parse_arguments_from_element(
+                    process_step.html_element,
+                    ['condition', 'cond-input', 'true-input', 'false-input']
+                )
+                cg_true = cg_steps[int(true_idx)]
+                cg_false = cg_steps[int(false_idx)]
+                cg_cond = cg_steps[int(cond_idx)]
+                
+                cg_out = if_else(cg_true, cg_false, cg_cond, condition)
+                
+            elif process_step.process_type == "adjust-rgb":
+                shifts = [
+                    float(val)
+                    for val in parse_arguments_from_element(
+                        process_step.html_element,
+                        ['r-shift', 'g-shift', 'b-shift']
+                    )
+                ]
+                
+                cg_out = adjust_rgb(cg_in, *shifts)
+                
+            elif process_step.process_type == "adjust-hsv":
+                shifts = [
+                    float(val)
+                    for val in parse_arguments_from_element(
+                        process_step.html_element,
+                        ['h-shift', 's-shift', 'v-shift']
+                    )
+                ]
+                
+                cg_out = adjust_hsv(cg_in, *shifts)
+                
+            elif process_step.process_type == "brightness-contrast":
+                shifts = [
+                    float(val)
+                    for val in parse_arguments_from_element(
+                        process_step.html_element,
+                        ['bright-shift', 'con-shift']
+                    )
+                ]
+                
+                cg_out = brightness_contrast(cg_in, *shifts)
+                
+            elif process_step.process_type == "custom":
+                expressions = parse_arguments_from_element(
+                    process_step.html_element,
+                    ['new-r', 'new-g', 'new-b']
+                )
+                
+                cg_out = custom_rgb_adjust(cg_in, *expressions)
+                
+            else:
+                raise NotImplementedError(f"unknown proess type '{process_step.process_type}'")
+            if cg_out is None:
+                raise RuntimeError(f"`cg_out` did not get set with process of type '{process_step.process_type}'")
+                
+            cg_steps.append(cg_out)
         
-        # Run the step
-        if process_step.process_type == "8-value-recolor":
-            colors = parse_arguments_from_element(process_step.html_element, ['black','white','red','green','blue','yellow','magenta','cyan'])
-            colors_scalar = [parse_color(c) for c in colors]
-            cg_out = linear_recolor(cg_in, colors_scalar)
-            
-        elif process_step.process_type == "simple-recolor":
-            colors = parse_arguments_from_element(process_step.html_element, ['black','white'])
-            colors_scalar = [parse_color(c) for c in colors]
-            cg_out = simple_recolor(cg_in, *colors_scalar)
-            
-        elif process_step.process_type == "recenter-colors":
-            cg_out = rescale_to_fill_range(cg_in)
-            
-        elif process_step.process_type == "fill":
-            color = parse_color(
-                parse_arguments_from_element(
-                    process_step.html_element,
-                    ['color']
-                )[0]
+        except Exception as e:
+            show_error_exception(
+                e,
+                prefix=f"Error parsing step {i}:",
+                show_error_type=False,
             )
-            cg_out = get_filled_colorgrade(color)
-            
-        elif process_step.process_type == "if-else":
-            condition, cond_idx, true_idx, false_idx = parse_arguments_from_element(
-                process_step.html_element,
-                ['condition', 'cond-input', 'true-input', 'false-input']
-            )
-            cg_true = cg_steps[int(true_idx)]
-            cg_false = cg_steps[int(false_idx)]
-            cg_cond = cg_steps[int(cond_idx)]
-            
-            cg_out = if_else(cg_true, cg_false, cg_cond, condition)
-            
-        elif process_step.process_type == "adjust-rgb":
-            shifts = [
-                float(val)
-                for val in parse_arguments_from_element(
-                    process_step.html_element,
-                    ['r-shift', 'g-shift', 'b-shift']
-                )
-            ]
-            
-            cg_out = adjust_rgb(cg_in, *shifts)
-            
-        elif process_step.process_type == "adjust-hsv":
-            shifts = [
-                float(val)
-                for val in parse_arguments_from_element(
-                    process_step.html_element,
-                    ['h-shift', 's-shift', 'v-shift']
-                )
-            ]
-            
-            cg_out = adjust_hsv(cg_in, *shifts)
-            
-        elif process_step.process_type == "brightness-contrast":
-            shifts = [
-                float(val)
-                for val in parse_arguments_from_element(
-                    process_step.html_element,
-                    ['bright-shift', 'con-shift']
-                )
-            ]
-            
-            cg_out = brightness_contrast(cg_in, *shifts)
-            
-        elif process_step.process_type == "custom":
-            expressions = parse_arguments_from_element(
-                process_step.html_element,
-                ['new-r', 'new-g', 'new-b']
-            )
-            
-            cg_out = custom_rgb_adjust(cg_in, *expressions)
-            
-        else:
-            raise NotImplementedError("")
-        
-        if cg_out is None:
-            raise RuntimeError(f"`cg_out` did not get set with process of type '{process_step.process_type}'")
-            
-        cg_steps.append(cg_out)
+            raise
         
     result = cg_steps[-1]
     write_colorgrade(result)
@@ -454,4 +484,26 @@ def write_colorgrade(cg):
         for j in range(16):
             canvas_ctx.fillStyle = f"rgb({flat_cg[i,j,0]},{flat_cg[i,j,1]},{flat_cg[i,j,2]})"
             canvas_ctx.fillRect(i, j, 1, 1)
+    
+def show_error_exception(e, prefix="Error:", post="", show_error_type=True):
+    """
+    Displays the exception in the error box
+    """
+    if len(prefix) > 0:
+        prefix = prefix+' '
+    if len(post) > 0:
+        post = ' ' + post
+    if show_error_type:
+        class_name = f"{e.__class__.__name__}: "
+    else:
+        class_name = ""
+    message = f"{prefix}{class_name}{str(e)}.{post}"
+    show_error_text(message)
+    
+def show_error_text(error_text):
+    """
+    Displays the given text to the error box
+    """
+    error_message.innerHTML = error_text
+    error_message.style.display = 'block'
     
